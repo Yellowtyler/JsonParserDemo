6dad01bbaa4db13a6023e7199634b4d12a0efc77
Daniil <davydovdanya18@gmail.com>
2020-06-19 14:36:52 +0300

    added classes Label, Status, Task

--- src/main/java/com/vtb/core/entities/Task.java
+++ src/main/java/com/vtb/core/entities/Task.java
@@ -1,4 +1,61 @@
 package com.vtb.core.entities;
 
 public class Task {
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Status status;
+
+    private Label label;
+
+    public Task(Long id, String name, String description, Status status, Label label) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.label = label;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Label getLabel() {
+        return label;
+    }
+
+    public void setLabel(Label label) {
+        this.label = label;
+    }
 }
