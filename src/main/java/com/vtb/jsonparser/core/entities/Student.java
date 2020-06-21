19bf07bf5ded704cb9e82a24f1cc3c11273a9faf
FMW14 <fmw14@yandex.ru>
2020-06-21 17:23:01 +0300

    added non null check for Student, annotations optimized

--- src/main/java/com/vtb/jsonparser/core/entities/Student.java
+++ src/main/java/com/vtb/jsonparser/core/entities/Student.java
@@ -1,107 +1,57 @@
 package com.vtb.jsonparser.core.entities;
 
-import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NonNull;
 import lombok.ToString;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @JsonPropertyOrder({"id", "firstName", "secondName", "phone", "email", "tasks"})
 @EqualsAndHashCode
 @ToString
+@Data
 public class Student {
-
+    @JsonProperty("id")
+    @NonNull
     private Long id;
+
+    @JsonProperty("firstName")
+    @NonNull
     private String firstName;
+
+    @JsonProperty("secondName")
+    @NonNull
     private String secondName;
+
     @JsonIgnore
+    @NonNull
     private String password;
+
+    @JsonProperty("phone")
+    @NonNull
     private String phone;
+
+    @JsonProperty("email")
+    @NonNull
     private String email;
+
     @JsonProperty("tasks")
+    @NonNull
     private List<Task> subTasks;
 
-    public Student(){}
+    public Student() {
+    }
 
-    @JsonCreator
-    public Student( @JsonProperty("id") Long id, @JsonProperty("firstName") String firstName,
-                    @JsonProperty("secondName") String secondName,
-                    @JsonProperty("phone") String phone, @JsonProperty("email")String email,
-                    @JsonProperty("tasks") List<Task> tasks) {
+    public Student(Long id, String firstName, String secondName, String phone, String email, List<Task> tasks) {
         this.id = id;
         this.firstName = firstName;
         this.secondName = secondName;
         this.phone = phone;
         this.email = email;
-        this.subTasks = tasks;
-    }
-
-    @JsonGetter("id")
-    public Long getId() {
-        return id;
-    }
-
-    @JsonSetter("id")
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @JsonGetter("firstName")
-    public String getFirstName() {
-        return firstName;
-    }
-
-    @JsonSetter("firstName")
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @JsonGetter("secondName")
-    public String getSecondName() {
-        return secondName;
-    }
-
-    @JsonSetter("secondName")
-    public void setSecondName(String secondName) {
-        this.secondName = secondName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @JsonGetter("phone")
-    public String getPhone() {
-        return phone;
-    }
-
-    @JsonSetter("phone")
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    @JsonGetter("email")
-    public String getEmail() {
-        return email;
-    }
-
-    @JsonSetter("email")
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @JsonGetter("tasks")
-    public List<Task> getTasks() {
-        return subTasks;
-    }
-
-    @JsonSetter("tasks")
-    public void setTasks(ArrayList<Task> tasks) {
         this.subTasks = tasks;
     }
 }
